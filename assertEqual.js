3db53ebf89fd83404f46199a19ce81afa8bc507b
Konstantin <kostakv@outlook.com>
2022-03-27 17:05:20 +0000

    Comparing values function

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,12 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
+
+  //First condition if the two inputs are equal
   if (actual === expected){
-    return '✅✅✅ Assertion Passed: '+actual +' === '+expected
+    return '✅✅✅ Assertion Passed: '+actual +' === '+expected 
   }
+
+  //Else statment if it's !==
   else {
     return '🛑🛑🛑 Assertion Failed: '+actual +' !== '+expected
   }
