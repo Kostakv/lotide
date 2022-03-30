0110354266e6b1c23919fa3e98ce92b11f6653b9
Konstantin <kostakv@outlook.com>
2022-03-30 19:37:56 +0000

    [feat]: Added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  //First condition if the two inputs are equal
+  if (actual === expected){
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}` 
+  }
+
+  //Else statment if it's !==
+  else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+  }
+};
+
+//FUNCTION that has two arguments object and value
+const findKeyByValue = function (object, value){
+  for (const i of Object.keys(object)) {
+    
+    if (object[i] === value){
+      return i
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
