3be689304298a78c1f9d7ad2c42e486b92e1782f
Konstantin <kostakv@outlook.com>
2022-04-14 21:35:38 +0000

    [feat]: added assertArrayEqualTest

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,21 +1,5 @@
-//Function that takes in two arrays and checks if it's equal to eachother
-const eqArrays = function (firstArray, secondArray){
-  var pass = 0 // Variable to determine if it's true or false
-  for (var i = 0; i < firstArray.length; i++){
-    if (firstArray[i] !== secondArray[i]){
-      pass = 1
-    }
-  
-  }
-
-  // If/else statment for result
-  if (pass === 1){
-    return false
-  }
-  else {
-    return true
-  }
-}
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./eqArrays');
 
 const assertArrayEqual = function (arrayOne, arrayTwo){
   //If statement that gets the returned value from eqArrays
@@ -30,4 +14,6 @@ const assertArrayEqual = function (arrayOne, arrayTwo){
   
 
 }
-console.log(assertArrayEqual([50,50,50,50],[50,50,50,50]))
+
+
+module.exports = assertArrayEqual;
---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,2 @@
+const assertEqual = require('../assertArrayEqual');
+
