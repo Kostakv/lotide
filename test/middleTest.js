1c5f4534dd383ec15c3196397912f15b039f785d
Konstantin <kostakv@outlook.com>
2022-04-14 22:26:51 +0000

    [update]: updated middleTest with mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,2 +1,9 @@
 const middle = require('../middle');
-const assertArrayEqual = require('../assertArrayEqual');
+
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
